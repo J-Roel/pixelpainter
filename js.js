cf4fe2363ebbe4a74fe45b7538e5d5bd399b40c3
Jeremy Roelfs <jeremyroelfs@icloud.com>
2015-11-14 23:17:43 -0700

    update

--- js.js
+++ js.js
@@ -3,26 +3,43 @@ var PixelPainter = function() {
 
 
 	function buildCanvas(x,y){
-		console.log('Reticulating Splines');
-		var canvas = document.getElementById("canvas");
-		for (var i = 1; i <= x; i++) {
-	    	var divX = document.createElement('div');
-	    	for (var j = 1; j <= y; j++) {
-	        	var divY = document.createElement('div');
+		if(x > 100){ x = 100; }
+		if(x <= 0){ x = 1; }
+		if(y > 100){ y = 100; }
+		if(y <= 0){ y = 1; }
+
+		nX = parseInt(x);
+		nY = parseInt(y);
+
+
+		console.log('Reticulating Splines');//simcity... loved that game
+		for (var i = 1; i <= nX; i++) { //loop through our rows
+	    	var divX = document.createElement('div');//Make a div element
+	    	for (var j = 1; j <= nY; j++) { //loop through our columns
+	        	var divY = document.createElement('div'); //
 	        		divY.className = 'pixel';
 	        		divX.appendChild(divY);
 	   		}
 	   		canvas.appendChild(divX);
 		}
+		newSize = (25*nX) + 10;
+		var strSize = newSize.toString() + "px";
+		
+		canvas.style.width = strSize;
+		console.log("Canvas width: " , canvas.style.width, " : " + strSize);
 
 
 	}
 	
 
-	//Define vars
+	//Define vars - get DOM elements here so we don't have to keep asking for them
+	//every time we call a function.
 	var curColor = 'rgba(0,0,0,1)'; //default color
 	var colorMode = 1; //default mode
-	var eraser = false;
+	var eraser = false; //set default
+	var palette = document.getElementById("palette");// the parent of our color elements
+	var divs = document.getElementsByClassName('color'); //all of the color elements
+	var canvas = document.getElementById("canvas"); //the parent of our pixels to "draw" on
 	var paint  = true;
 	var cPalette = [ '#33FFFF','#33FFCC','#33FF99','#33FF66','#33FF33','#33FF00'
 					,'#33CCFF','#33CCCC','#33CC99','#33CC66','#33CC33','#33CC00'
@@ -35,13 +52,18 @@ var PixelPainter = function() {
 	function buildPalette() {
 		//Build our color palette
 		console.log('Building Palette', cPalette.length);
-			var palette = document.getElementById("palette");
-			for (var i = 0; i < cPalette.length; i++) {
+			for (i = 0; i < cPalette.length; i++) {
 				var colorS = document.createElement('span');
 				colorS.className = 'color';
-				colorS.style.background = cPalette[i];
+				colorS.style.backgroundColor = cPalette[i];
 				palette.appendChild(colorS);
 	   		}
+	}
+	function addToPalette(str) {
+			var colorS = document.createElement('span');
+			colorS.className = 'color';
+			colorS.style.backgroundColor = str;
+			palette.appendChild(colorS);
 	}
 
 
@@ -50,37 +72,39 @@ var PixelPainter = function() {
 		console.log(event);
 		if (event.which === 1) {
 			
-			if(eraser) {
-					event.target.style.background = 'transparent'
+			if(event.target.className === "pixel") //check to see if we clicked on a pixel
+			{
+				if(eraser) {
+					event.target.style.backgroundColor = '';
 					event.target.style.border = "1px solid #000";
 					event.target.style.borderRadius = "4px";
-			} else if (paint){
-
-				if(event.target.className === "pixel") 
-				{
+				} else {
 					event.target.style.border = "none";
 					event.target.style.borderRadius = "0";
-	 				event.target.style.background = curColor;
-				} else if(event.target.className === "color")
-				{
-					curColor = event.target.style.backgroundColor;
-					
-					var divs = document.getElementsByClassName('color');
-					for(var i = 0; i < divs.length; i++ ) {
-						divs[i].style.border = "none";
-						divs[i].style.borderRadius = "0";
-					}
-					event.target.style.border = "1px solid #000";
-					event.target.style.borderRadius = "10px";
+ 					event.target.style.backgroundColor = curColor;
+ 				}
+ 				
+
+			} else if(event.target.className === "color") //or on a color
+			{
+				curColor = event.target.style.backgroundColor;
+				
+				
+				for(var i = 0; i < divs.length; i++ ) {
+					divs[i].style.border = "none";
+					divs[i].style.borderRadius = "50%";
 				}
+				event.target.style.border = "3px solid #fff";
+				event.target.style.borderRadius = "5px";
+
 			}
 		}
 	};
+	window.addEventListener('mousedown', pixelFunk);
 
 
 
 
-	window.addEventListener('click', pixelFunk);
 
 	var cPaint = document.getElementById('cMode1');
 	cPaint.addEventListener('click', function f() {
@@ -104,7 +128,18 @@ var PixelPainter = function() {
 			paint = false;
 	});
 
+	//MAKE GRID
+	var makeGrid = document.getElementById("makeGrid")
+	makeGrid.addEventListener('click',function func() {
+		var pixelS = document.getElementsByClassName('pixel');
+		for(var i = 0; i <= pixelS.length+2; i++) {
+			canvas.removeChild(canvas.childNodes[0]);
+		}
 
+		var x = document.getElementById('gridx').value;
+		var y = document.getElementById('gridy').value;
+		buildCanvas(x,y);
+	});
 
 
 	//CUSTOM COLOR
@@ -116,8 +151,12 @@ var PixelPainter = function() {
 		str += 'rgba(';
 		str += nC;
 		str += ")"
-		console.log(nC);
+		console.log(str);
 		curColor = str;
+		
+		//add this to our palette
+		cPalette.push(str);
+		addToPalette(str);
 	});
 
 
@@ -128,7 +167,7 @@ var PixelPainter = function() {
 		console.log(divs.length);
 		for(var i = 0; i < divs.length; i++)
 		{
-			divs[i].style.background = 'transparent'
+			divs[i].style.backgroundColor = 'transparent'
 			divs[i].style.border = "1px solid #000";
 			divs[i].style.borderRadius = "4px";			
 
@@ -140,26 +179,25 @@ var PixelPainter = function() {
 	var customC = document.getElementById('toggleGrid');
 	toggleGrid.addEventListener('click', function customColor(event) {
 		var divs = document.getElementsByClassName('pixel');
-		var chk = divs[0].style.border;//check an initial pixel to see if grid is active
+		var chk = divs[0].style.border;
 		console.log(chk);
 		if(chk == "none")
 		{	
+			console.log("adding");
 			for(i = 0; i < divs.length; i++)
 			{
-				if(divs[i].style.border != 'transparent')
+				if(divs[i].style.backgroundColor == '')
 				{
 					divs[i].style.border = "1px solid #000";
 					divs[i].style.borderRadius = "4px";
 				}
 			}
 		} else {	
+			console.log("removing");
 			for(i = 0; i < divs.length; i++)
 			{
-				if(divs[i].style.border != 'transparent')
-				{
 					divs[i].style.border = "none";
 					divs[i].style.borderRadius = "0";
-				}
 			}
 		}
 
@@ -171,7 +209,15 @@ var PixelPainter = function() {
 //takes 3 arguments
 // x - width of the canvas
 // y - height of the canvas
-buildCanvas(20,25);
+buildCanvas(100,100);//30 is the default grid size
 buildPalette();
+
+
+
+
+
+
+
+
 
 }();
