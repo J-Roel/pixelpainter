39a91c16f5aee4a3ec45a7ca7873063ebb6cc2fa
Jeremy Roelfs <jeremyroelfs@icloud.com>
2015-11-15 13:10:26 -0700

    Added 2 features, fixed 2 other features, added comments, moved all variables to make less calls

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pixelpainter
+`Update 1:
+`
+`Update 2:
+`			- Added zoom function, and make grid.
+`			- fixed Toggle Grid and custom paint
+`			- rearranged UI to prevent accdently clicking on clear/make grid
+`			- Assign all reused variables at top of file and only set them
+`				when necessary... avoids calling document.getElement every function
+`			-Added comments
--- js.js
+++ js.js
@@ -1,46 +1,23 @@
-var PixelPainter = function() {
+window.onload = function(){ //program contianer function
 
 
-
-	function buildCanvas(x,y){
-		if(x > 100){ x = 100; }
-		if(x <= 0){ x = 1; }
-		if(y > 100){ y = 100; }
-		if(y <= 0){ y = 1; }
-
-		nX = parseInt(x);
-		nY = parseInt(y);
-
-
-		console.log('Reticulating Splines');//simcity... loved that game
-		for (var i = 1; i <= nX; i++) { //loop through our rows
-	    	var divX = document.createElement('div');//Make a div element
-	    	for (var j = 1; j <= nY; j++) { //loop through our columns
-	        	var divY = document.createElement('div'); //
-	        		divY.className = 'pixel';
-	        		divX.appendChild(divY);
-	   		}
-	   		canvas.appendChild(divX);
-		}
-		newSize = (25*nX) + 10;
-		var strSize = newSize.toString() + "px";
-		
-		canvas.style.width = strSize;
-		console.log("Canvas width: " , canvas.style.width, " : " + strSize);
-
-
-	}
+	/*
+		Pixel Painter - by jroel
+	*/
 	
 
 	//Define vars - get DOM elements here so we don't have to keep asking for them
 	//every time we call a function.
 	var curColor = 'rgba(0,0,0,1)'; //default color
 	var colorMode = 1; //default mode
+	var x = '32'; //default grid width
+	var y = '32'; //default grid height
 	var eraser = false; //set default
 	var palette = document.getElementById("palette");// the parent of our color elements
-	var divs = document.getElementsByClassName('color'); //all of the color elements
-	var canvas = document.getElementById("canvas"); //the parent of our pixels to "draw" on
-	var paint  = true;
+	var colors = document.getElementsByClassName('color'); //all of the color elements
+	var canvas = document.getElementById("canvas"); //the parent of our pixels
+	var pixels = document.getElementsByClassName('pixel'); //all of the pixel elements
+	var zoom = 25;
 	var cPalette = [ '#33FFFF','#33FFCC','#33FF99','#33FF66','#33FF33','#33FF00'
 					,'#33CCFF','#33CCCC','#33CC99','#33CC66','#33CC33','#33CC00'
 					,'#3399FF','#3399CC','#339999','#339966','#339933','#339900'
@@ -49,54 +26,141 @@ var PixelPainter = function() {
 					,'#3300FF','#3300CC','#330099','#330066','#330033','#330000'
 					,'#00FFFF','#00FFCC','#00FF99','#00FF66','#00FF33','#000000'
 					,'#ffffff','#CCCCCC','#DDDDDD'];
-	function buildPalette() {
-		//Build our color palette
-		console.log('Building Palette', cPalette.length);
-			for (i = 0; i < cPalette.length; i++) {
-				var colorS = document.createElement('span');
-				colorS.className = 'color';
-				colorS.style.backgroundColor = cPalette[i];
-				palette.appendChild(colorS);
+	
+
+
+	//BUILD CANVAS ----------------------------------------------------------------
+	function buildCanvas(x,y){
+		if(x > 128){ x = 128; } //check our numbers and hold them in a range.
+		if(x <= 0){ x = 4; }
+		if(y > 128){ y = 128; }
+		if(y <= 0){ y = 4; }
+
+		nX = parseInt(x); //change to numbers so we can use them in the
+		nY = parseInt(y); //process below
+
+		console.log('Reticulating Splines');
+		for (var i = 1; i <= nX; i++) { //loop through our rows
+	    	var divX = document.createElement('div');//Make a div element
+	    	for (var j = 1; j <= nY; j++) { //loop through our columns
+	        	var divY = document.createElement('div'); // make another div
+	        		divY.className = 'pixel'; //change that div's class to pixel
+	        		divX.appendChild(divY); //append to our X (aka row)
 	   		}
+	   		canvas.appendChild(divX); //then append to our canvas
+		}
+
+		//Next refactor the width of our canvas to accomodate the new size
+		/*
+			This should be straightforward, but I am having
+			problems forcing the canvas width to a proper size.
+			So I push 500 on the end of it so the inline-block so it
+			doesn't mess up how the grid sets. A conflict with the width and inline-block.
+		*/
+		newSize = (zoom*nX) + nX; //zoom is our 'global' setting, * user info from html input		
+		var strSize = newSize.toString() + "px"; //set our data to a string and append px to the end
+		canvas.style.width = strSize; //finally adjust our canvas width property.
 	}
+
+	//MAKE GRID -- our listner function for rebuilding the canvas.-----------------
+	var makeGrid = document.getElementById("makeGrid")
+	makeGrid.addEventListener('click',function func() {
+		for(var i = 0; i <= pixels.length+2; i++) { //loop through pixels
+			canvas.removeChild(canvas.childNodes[0]); //delete the pixels
+		}
+
+		var x = document.getElementById('gridx').value; //get our values from the inputs
+		var y = document.getElementById('gridy').value;
+		buildCanvas(x,y); //call buildCanvas function to build the canvas with the new inputs
+	});
+
+
+	//BUILD PALETTE -------------------------------------
+	function buildPalette() {
+		console.log('Building Palette', cPalette.length);
+			for (i = 0; i < cPalette.length; i++) { //loop through our color palette array
+				var colorS = document.createElement('div'); //create a new element for our color
+				colorS.className = 'color';//assign the class to our element.
+				colorS.style.backgroundColor = cPalette[i]; //then color it per our palette.
+				palette.appendChild(colorS); //append this to our color container "colorS"
+	   		}
+
+	   		var pixels = document.getElementsByClassName('pixel');
+	}
+
+	//ADD TO PALETTE--------------------------------------
+	// This works in relation to the listener customColor below
+	// this function is different than buildPalette... 
+	// theres no need to rebuild the palette when are simply adding an element.
 	function addToPalette(str) {
-			var colorS = document.createElement('span');
-			colorS.className = 'color';
-			colorS.style.backgroundColor = str;
-			palette.appendChild(colorS);
+			var colorS = document.createElement('div');//build a single div for our new color
+			colorS.className = 'color'; //Set it's className
+			colorS.style.backgroundColor = str; //str is passed from our customColor listner function
+			palette.appendChild(colorS); //Once our new color is setup... append it to our colorS container
+
 	}
+	//CUSTOM COLOR
+	var customColor = document.getElementById('customColor'); //our listener to customColor
+	customColor.addEventListener('click', function customColor(event) {
+		var nC = window.prompt("Enter color: ie: R,G,B,A");//TODO, make an input box instead
+		
+		//Next make up the rgba string we'll pass to our palette so we can setup a color.
+		var str = ''; //make sure the string has nothing in it when we make it.
+		str += 'rgba('; //we add rgba( to the front of the string.
+		str += nC; //add our input from user. TODO: needs validation here.
+		str += ")" //add the ending on the string.
+		
+		curColor = str; //pass the string to our "global" variable curColor for painting
+		
+		
+		cPalette.push(str);//add this to our palette array
+		//next we use the addToColor function to add this to our colorS container 
+		//The reason I did this, is eventually, you can load color palettes
+		//and add various colors by like a color picker, so I broke this up into
+		//different functions.
+		addToPalette(str); 
+	});
 
 
-	//MOUSE ACTION TO CHANGE THE PIXELS
+	//MOUSE ACTION TO CHANGE THE PIXELS  ---------------------------
+	//this is our main action handler. 
 	function pixelFunk(event) {
 		console.log(event);
 		if (event.which === 1) {
 			
 			if(event.target.className === "pixel") //check to see if we clicked on a pixel
 			{
-				if(eraser) {
-					event.target.style.backgroundColor = '';
-					event.target.style.border = "1px solid #000";
-					event.target.style.borderRadius = "4px";
-				} else {
-					event.target.style.border = "none";
-					event.target.style.borderRadius = "0";
- 					event.target.style.backgroundColor = curColor;
- 				}
- 				
+				switch(colorMode)//Switch between our colorMode to determine what action to take
+				{
+					case 1://Paint
+						event.target.style.border = "none";
+						event.target.style.borderRadius = "0";
+ 						event.target.style.backgroundColor = curColor;
+ 						break;
 
-			} else if(event.target.className === "color") //or on a color
+ 					case 5: //Eraser
+						event.target.style.backgroundColor = '';
+						event.target.style.border = "1px solid #000";
+						event.target.style.borderRadius = "4px";
+						break;
+ 				}
+
+			} else if(event.target.className === "color") //if not a pixel...
 			{
-				curColor = event.target.style.backgroundColor;
+				curColor = event.target.style.backgroundColor; //simple to get the color
 				
-				
-				for(var i = 0; i < divs.length; i++ ) {
-					divs[i].style.border = "none";
-					divs[i].style.borderRadius = "50%";
+				//loop through palette and change our styles to see what
+				//we have selected... thanks Alya!
+
+				//First we have to deselect all of the colors
+				for(var i = 0; i < colors.length; i++ ) { 
+					colors[i].style.border = "none";
+					colors[i].style.borderRadius = "50%";
 				}
+
+				//Then select the one we clicked on.
 				event.target.style.border = "3px solid #fff";
 				event.target.style.borderRadius = "5px";
-
 			}
 		}
 	};
@@ -105,102 +169,102 @@ var PixelPainter = function() {
 
 
 
-
+	//Listener function... changes our colorMode to paint
 	var cPaint = document.getElementById('cMode1');
 	cPaint.addEventListener('click', function f() {
-			paint = true;
-			eraser = false;
+			colorMode = 1;
 	});
+
+	//Listener function... changes our colorMode to Roller
 	var mRoller = document.getElementById('cMode2');
 	cMode2.addEventListener('click', function f() {
 			colorMode = 2;
 	});
 
+	//Listner function... changes our colorMode to Erase
 	var mBox = document.getElementById('cMode3');
 	mBox.addEventListener('click', function f() {
 			colorMode = 3;
 	});
 
-	//ERASER
+	//Listner function... changes our colorMode to Erase
 	var cEraser = document.getElementById('cEraser');
 	cEraser.addEventListener('click', function f() {
-			eraser = true;
-			paint = false;
-	});
-
-	//MAKE GRID
-	var makeGrid = document.getElementById("makeGrid")
-	makeGrid.addEventListener('click',function func() {
-		var pixelS = document.getElementsByClassName('pixel');
-		for(var i = 0; i <= pixelS.length+2; i++) {
-			canvas.removeChild(canvas.childNodes[0]);
-		}
-
-		var x = document.getElementById('gridx').value;
-		var y = document.getElementById('gridy').value;
-		buildCanvas(x,y);
-	});
-
-
-	//CUSTOM COLOR
-	var customC = document.getElementById('customColor');
-	customC.addEventListener('click', function customColor(event) {
-		var nC = window.prompt("Enter color: ie: R,G,B,A");
-		var str = '';
-
-		str += 'rgba(';
-		str += nC;
-		str += ")"
-		console.log(str);
-		curColor = str;
-		
-		//add this to our palette
-		cPalette.push(str);
-		addToPalette(str);
+			colorMode = 5;
 	});
 
 
 	//CLEAR PICTURE
+	//just loops through the canvas elements and changes them back
+	//to orginal styles
 	var cClear = document.getElementById('cClear');
 	cClear.addEventListener('click', function customColor(event) {
-		var divs = document.getElementsByClassName('pixel');
-		console.log(divs.length);
-		for(var i = 0; i < divs.length; i++)
+		
+		for(var i = 0; i < pixels.length; i++)
 		{
-			divs[i].style.backgroundColor = 'transparent'
-			divs[i].style.border = "1px solid #000";
-			divs[i].style.borderRadius = "4px";			
-
+			pixels[i].style.backgroundColor = '';
+			pixels[i].style.border = "1px solid #000";
+			pixels[i].style.borderRadius = "4px";			
 		}
 	});
 
 
 	//TOGGLE GRID
-	var customC = document.getElementById('toggleGrid');
+	//This listener loops through the canvas elements and
+	//if an element has a backgroudColor, it passes by it,
+	//if it doesn't have a backgroundColor... it removes the border
+	var toggleGrid = document.getElementById('toggleGrid');
 	toggleGrid.addEventListener('click', function customColor(event) {
-		var divs = document.getElementsByClassName('pixel');
-		var chk = divs[0].style.border;
-		console.log(chk);
-		if(chk == "none")
-		{	
-			console.log("adding");
-			for(i = 0; i < divs.length; i++)
-			{
-				if(divs[i].style.backgroundColor == '')
-				{
-					divs[i].style.border = "1px solid #000";
-					divs[i].style.borderRadius = "4px";
-				}
-			}
-		} else {	
-			console.log("removing");
-			for(i = 0; i < divs.length; i++)
-			{
-					divs[i].style.border = "none";
-					divs[i].style.borderRadius = "0";
+		
+
+		var chk ='';
+		for(var i = 0; i <= pixels.length;i++) {
+			chk = pixels[i].style.border; //get style.border info from pixel
+			
+			if(chk == "none") //if the pixel border style is set to none we know they are off.
+			{	
+				console.log("adding"); //So we add them.
+					if(pixels[i].style.backgroundColor == '') //check of there's no background
+					{	 	
+						//don't want to mess with elements that are painted.
+						pixels[i].style.border = "1px solid #000"; //add the border
+						pixels[i].style.borderRadius = "4px";
+					}
+			} else {	
+				console.log("removing");
+					pixels[i].style.border = "none";
+					pixels[i].style.borderRadius = "0";
 			}
 		}
 
+	});
+
+	//ZOOM --------------------------------------------------------------
+	//pretty straight forward function.... has break points to zoom in
+	//more can be added. Even though I have having problems again with the
+	//width functionality of the div element and forcing it to hold the
+	//inline-block elements correctly.
+	var cZoom = document.getElementById('zoom');
+	cZoom.addEventListener('click', function f() {
+
+			
+			switch(zoom) {
+				case 10: zoom = 10; break;
+				case 15: zoom = 25; break;
+				case 25: zoom = 35; break;
+				case 35: zoom = 45; break;
+				case 45: zoom = 55; break;
+				case 55: zoom = 15; break;
+				default: zoom = 25;
+			}
+			
+			for(var i = 0; i < pixels.length; i++ ) {
+				pixels[i].style.width = zoom.toString() + "px";
+				pixels[i].style.height = zoom.toString() + "px";
+			}
+
+			newSize = (zoom*x)+ x;
+			canvas.style.width = newSize.toString() + "px";;
 	});
 
 
@@ -209,7 +273,7 @@ var PixelPainter = function() {
 //takes 3 arguments
 // x - width of the canvas
 // y - height of the canvas
-buildCanvas(100,100);//30 is the default grid size
+buildCanvas(x,y);//x = width, y = height of canvas per pixel
 buildPalette();
 
 
